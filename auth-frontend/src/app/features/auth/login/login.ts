e0922cbc9ba4eebc69ed8ae0711e995423fe6b4d
Harsimar08 <harsimark647@gmail.com>
2026-07-16 17:52:48 +0530

    Initial commit

---
+++ auth-frontend/src/app/features/auth/login/login.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+}
